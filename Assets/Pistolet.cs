10ee497b56a8f4a28a05f33161a25ac8764c06c4
AGagnon <41961082+Agagnon01@users.noreply.github.com>
2026-03-04 09:51:22 -0500

    Ajout du pistolet

---
+++ Assets/Pistolet.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Readers;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class Pistolet : MonoBehaviour
+{
+
+    private XRGrabInteractable grabInteractable;
+    private ParticleSystem gaz;
+    private bool gazState = false;
+
+    void Awake()
+    {
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        gaz = GetComponentInChildren<ParticleSystem>();
+        grabInteractable.activated.AddListener(Activate);
+    }
+
+    void OnEnable()
+    {
+       
+    }
+
+    void OnDisable()
+    {
+       
+    }
+
+    public void Activate(ActivateEventArgs args)
+    {
+        print("Hello");
+        ToggleGaz();
+        //gaz.Play();
+    }
+
+    private void ToggleGaz()
+    {
+        if (gaz == null)
+            return;
+
+        if (gazState)
+        {
+            gaz.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            Debug.Log("Stop");
+        }
+        else
+        {
+            gaz.Play();
+            Debug.Log("Play");
+        }
+
+        gazState = !gazState;
+    }
+
+    void Update()
+    {
+
+        
+    }
+}
